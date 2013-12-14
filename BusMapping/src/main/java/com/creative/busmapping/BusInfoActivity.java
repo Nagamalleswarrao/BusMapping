986c92b794025410e96e5375bdc27961225b2b0b
Eshwar <nagamalleswarrao.ila@gmail.com>
2013-12-14 11:12:30 +0530

    List view i populated with some random stuff

---
+++ BusMapping/src/main/java/com/creative/busmapping/BusInfoActivity.java
@@ -0,0 +1,41 @@
+package com.creative.busmapping;
+
+/**
+ * Created by Eshwar on 12/12/13.
+ */
+
+import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import android.app.ListActivity;
+import android.widget.SimpleAdapter;
+public class BusInfoActivity extends ListActivity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_view);
+
+        SimpleAdapter adapter = new SimpleAdapter(
+                this,
+                list,
+                R.layout.bus_info,
+                new String[] {"busNumber","labelBusNumber"},
+                new int[] {R.id.text1,R.id.text2}
+        );
+        populateList();
+        setListAdapter(adapter);
+    }
+
+    static final ArrayList<HashMap<String,String>> list = new ArrayList<HashMap<String,String>>();
+
+    private void populateList() {
+        HashMap<String,String> temp = new HashMap<String,String>();
+        temp.put("busNumber","10H");
+        temp.put("labelBusNumber", "Bus Number :");
+        list.add(temp);
+
+    }
+
+}
